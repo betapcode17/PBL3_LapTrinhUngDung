6ee259ebd7be8c586c5b2ee3b35fa427b26b8e6b
betapcode17 <quocdat19991712@gmail.com>
2025-03-10 11:20:16 +0700

    Update

---
+++ VolunteerWebSite/Volunteer_website/wwwroot/js/Navbar_user.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const avatar = document.getElementById("profileDropdown");
+  const dropdownMenu = document.querySelector(".dropdown-menu"); // Menu dropdown
+
+  // Khi nhấn vào avatar, bật/tắt dropdown
+  avatar.addEventListener("click", function (event) {
+    event.stopPropagation(); // Ngăn chặn sự kiện click lan ra ngoài
+    dropdownMenu.classList.toggle("show");
+  });
+
+  // Khi nhấn vào bất kỳ đâu trong body, kiểm tra nếu không phải avatar thì ẩn dropdown
+  document.body.addEventListener("click", function (event) {
+    if (
+      !avatar.contains(event.target) &&
+      !dropdownMenu.contains(event.target)
+    ) {
+      dropdownMenu.classList.remove("show");
+    }
+  });
+});
