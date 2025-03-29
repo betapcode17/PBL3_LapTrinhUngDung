c2fd292e4d1e5fa7b21e98239dd65f23dce38878
Le Duong Huy <leduonghuy.7.8.5@gmail.com>
2025-03-29 18:23:27 +0700

    Xong phần Event và Dashboard của admin

--- VolunteerWebSite/Volunteer_website/obj/Debug/net9.0/Volunteer_website.AssemblyInfo.cs
+++ VolunteerWebSite/Volunteer_website/obj/Debug/net9.0/Volunteer_website.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Volunteer_website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90376fdfe4e310a86611a65b6752deec5fe44199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63208f97e19825b02e19996d081637f6e0e05ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("Volunteer_website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Volunteer_website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- VolunteerWebSite/Volunteer_website/wwwroot/AdminLayout/Chart.js
+++ VolunteerWebSite/Volunteer_website/wwwroot/AdminLayout/Chart.js
@@ -2,37 +2,182 @@ const ctx = document.getElementById("barChart").getContext("2d");
 new Chart(ctx, {
   type: "bar",
   data: {
-    labels: ["2013", "2014", "2014", "2015", "2016", "2017"],
+    labels: ["2019", "2020", "2021", "2022", "2023", "2024"],
     datasets: [
+      //Number of volunteers
       {
-        label: "Số lượng",
-        data: [10, 19, 3, 5, 2, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.3)",
-          "rgba(54, 162, 235, 0.3)",
-          "rgba(255, 206, 86, 0.3)",
-          "rgba(75, 192, 192, 0.3)",
-          "rgba(153, 102, 255, 0.3)",
-          "rgba(255, 159, 64, 0.3)",
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)",
-        ],
+        label: "",
+        data: [4, 6, 2, 8, 10, 21],
+        // backgroundColor: [
+        //   "rgba(255, 99, 132, 0.3)",
+        //   "rgba(54, 162, 235, 0.3)",
+        //   "rgba(255, 206, 86, 0.3)",
+        //   "rgba(75, 192, 192, 0.3)",
+        //   "rgba(153, 102, 255, 0.3)",
+        //   "rgba(255, 159, 64, 0.3)",
+        // ],
+        
+        // borderColor: [
+        //   "rgba(255, 99, 132, 1)",
+        //   "rgba(54, 162, 235, 1)",
+        //   "rgba(255, 206, 86, 1)",
+        //   "rgba(75, 192, 192, 1)",
+        //   "rgba(153, 102, 255, 1)",
+        //   "rgba(255, 159, 64, 1)",
+        // ],
+        backgroundColor: "rgba(255, 99, 132, 0.3)",
+        borderColor: "rgba(255, 99, 132, 1)",
+        
         borderWidth: 1,
       },
     ],
   },
   options: {
     responsive: true,
+    legend: {
+      display: false,
+    },
     scales: {
-      y: {
-        beginAtZero: true,
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Year',
+          fontColor: '#6c7293',
+          fontSize: 12,
+          fontStyle: 'bold'
+        },
+        gridLines: {
+          display: true,
+          color: 'rgba(204, 204, 204, 0.1)'
+        }
+      }],
+
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Events',
+          fontColor: '#6c7293',
+          fontSize: 12,
+          fontStyle: 'bold'
+        },
+        gridLines: {
+          display: true,
+          color: 'rgba(204, 204, 204, 0.1)'
+        },
+      
+        ticks: {
+          beginAtZero: true,
+          min: 0,
+        }
+      }]
+    },
+  },
+});
+
+new Chart(document.getElementById("barChart2").getContext("2d"), {
+  type: "bar",
+  data: {
+    labels: ["2019", "2020", "2021", "2022", "2023", "2024"],
+    datasets: [
+      //Number of volunteers
+      {
+        label: "",
+        data: [57, 48, 78, 65, 80, 90],
+        backgroundColor: "rgba(54, 162, 235, 0.3)",
+        borderColor: "rgba(54, 162, 235, 1)",
+        borderWidth: 1,
       },
+    ],
+  },
+  options: {
+    responsive: true,
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Year',
+          fontColor: '#6c7293',
+          fontSize: 12,
+          fontStyle: 'bold'
+        },
+        gridLines: {
+          display: true,
+          color: 'rgba(204, 204, 204, 0.1)'
+        }
+      }],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'volunteers',
+          fontColor: '#6c7293',
+          fontSize: 12,
+          fontStyle: 'bold'
+        },
+        gridLines: {
+          display: true,
+          color: 'rgba(204, 204, 204, 0.1)'
+        },
+        ticks: {
+          beginAtZero: true,
+          min: 0,
+        }
+      }]
+    },
+  },
+});
+new Chart(document.getElementById("barChart3").getContext("2d"), {
+  type: "bar",
+  data: {
+    labels: ["2019", "2020", "2021", "2022", "2023", "2024"],
+    datasets: [
+      {
+        label: "",
+        data: [100, 37, 50, 78, 80, 90],
+        backgroundColor: "rgba(255, 159, 64, 0.3)",
+        borderColor: "rgba(255, 159, 64, 1)",
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'Year',
+          fontColor: '#6c7293',
+          fontSize: 12,
+          fontStyle: 'bold'
+        },
+        gridLines: {
+          display: true,
+          color: 'rgba(204, 204, 204, 0.1)'
+        }
+      }],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'thousands VNĐ',
+          fontColor: '#6c7293',
+          fontSize: 12,
+          fontStyle: 'bold'
+        },
+        ticks: {
+          beginAtZero: true,
+          min: 0,
+        },
+        gridLines: {
+          display: true,
+          color: 'rgba(204, 204, 204, 0.1)'
+        }
+      }]
     },
   },
 });
