76d673c4c54c9c2ebd1c10b034802facd61f4cc6
ChibaoCodeLord <102230059@sv1.dut.udn.vn>
2025-05-07 21:21:21 +0700

    Thay đổi event, ...

--- VolunteerWebSite/Volunteer_website/obj/Debug/net9.0/Volunteer_website.AssemblyInfo.cs
+++ VolunteerWebSite/Volunteer_website/obj/Debug/net9.0/Volunteer_website.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Volunteer_website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8eba14facafb087d694aa0e31a2301a624cd0cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7cabe8ca933c93687ca63d5ee1a3a9d2c999e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Volunteer_website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Volunteer_website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
