bf423bf7e3e148c930d21f456f45e6a55400fd68
betapcode17 <quocdat19991712@gmail.com>
2025-06-01 16:55:56 +0700

    Bài báo

---
+++ VolunteerWebSite/Volunteer_website/ViewModels/Article.cs
@@ -0,0 +1,13 @@
+﻿namespace Volunteer_website.ViewModels
+{
+    public class Article
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Url { get; set; }
+        public string? PublishedAt { get; set; }
+        public string? Source { get; set; }
+        public string? UrlToImage { get; set; }
+
+    }
+}
