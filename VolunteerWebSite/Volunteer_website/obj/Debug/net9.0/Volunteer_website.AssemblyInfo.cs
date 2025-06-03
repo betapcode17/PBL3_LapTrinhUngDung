7c6f7390420d2bf15e8c42de12aaf43d10edfe81
ChibaoCodeLord <102230059@sv1.dut.udn.vn>
2025-06-03 23:21:41 +0700

    Merge

--- VolunteerWebSite/Volunteer_website/obj/Debug/net9.0/Volunteer_website.AssemblyInfo.cs
+++ VolunteerWebSite/Volunteer_website/obj/Debug/net9.0/Volunteer_website.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Volunteer_website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11005ad29301d1301cdb06dd0e1947f7133cbb03")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f332f889b3adc4de755ba59ee3c925c62722a03")]
 [assembly: System.Reflection.AssemblyProductAttribute("Volunteer_website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Volunteer_website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
